b302be3805b33c5fde4af0de8153c93fdc6685f7
Jean-Vincent Evanno <jv.evanno@gmail.com>
2016-05-25 11:35:13 +0200

    homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
+//= datepicker.js
+//= require jquery-fileupload/basic
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
+//= require attachinary_template
+//= require underscore
+//= require gmaps/google
 //= require_tree .
---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,10 @@
+$('.input-daterange').datepicker({
+    format: "dd/mm/yyyy",
+    startDate: "Today",
+    todayBtn: true,
+    language: "fr",
+    orientation: "bottom auto",
+    forceParse: false,
+    todayHighlight: true,
+    toggleActive: true
+});
