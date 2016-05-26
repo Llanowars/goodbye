65f190fe9a8ac3a59ea1d4c69b10515c355af0df
Jean-Vincent Evanno <jv.evanno@gmail.com>
2016-05-26 19:02:17 +0200

    modifications pdfkit

--- app/controllers/unsubs_controller.rb
+++ app/controllers/unsubs_controller.rb
@@ -10,8 +10,12 @@ class UnsubsController < ApplicationController
     html = render_to_string(layout: true, action: "new")
 
     kit = PDFKit.new(html, :page_size => 'Letter')
-    kit.stylesheets << Rails.root.to_s + "/public/assets/application-49e46c8435a0747ac9d8178ca011c4113a16f77937fd101831faaad71baa7427.css"
+    kit.stylesheets << Rails.root.to_s + "/public/" + view_context.asset_path("application.css")
     kit.to_file("#{Rails.root}/tmp/test.pdf")
+
+    # kit = PDFKit.new(html, :page_size => 'Letter')
+    # kit.stylesheets << Rails.root.to_s + "/public/assets/application-49e46c8435a0747ac9d8178ca011c4113a16f77937fd101831faaad71baa7427.css"
+    # kit.to_file("#{Rails.root}/tmp/test.pdf")
   end
 
   def create
@@ -39,6 +43,10 @@ class UnsubsController < ApplicationController
 
   def show
     @unsub = Unsub.find(params[:id])
+
+    kit = PDFKit.new(html, :page_size => 'Letter')
+    kit.stylesheets << Rails.root.to_s + "/public/assets/application-49e46c8435a0747ac9d8178ca011c4113a16f77937fd101831faaad71baa7427.css"
+    kit.to_file("#{Rails.root}/tmp/test.pdf")
   end
 
   private
