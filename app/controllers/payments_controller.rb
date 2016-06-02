5153f1501c2291c51eb1d0de988e5db6f50f5505
Jean-Vincent Evanno <jv.evanno@gmail.com>
2016-06-02 15:46:24 +0200

    pdf format good links email sender

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,18 +5,19 @@ class PaymentsController < ApplicationController
   end
 
   def show
+    @unsub = Unsub.find(params[:unsub_id])
   end
 
   def generate_pdf
 
     @unsub = Unsub.find(params[:unsub_id])
     @service = @unsub.service
-    html = render_to_string(layout: false, action: "create")
+    html = render_to_string(layout: false, action: "show")
 
     kit = PDFKit.new(html, :page_size => 'Letter')
     # kit.stylesheets << Rails.root.to_s + "/public" + view_context.asset_path("application.css")
 
-    send_data(kit.to_pdf, :filename => "#{@unsub.form_complete['firstname']}_#{@unsub.form_complete['lastname']}_#{@unsub.service.name.gsub(" ", "_")}.pdf",
+    send_data(kit.to_pdf, :filename => "#{@unsub.form_complete['firstname'].capitalize}_#{@unsub.form_complete['lastname'].capitalize}_#{@unsub.service.name.gsub(" ", "_")}.pdf",
                           :type => "application/pdf")
   end
 
@@ -37,10 +38,8 @@ class PaymentsController < ApplicationController
       currency:     'eur'
     )
 
-    #@order.update(payment: charge.to_json, state: 'paid')
 
-    UserMailer.unsub_email(@unsub.user).deliver_now
-    #redirect_to _path(@order)
+    UserMailer.unsub_email(@unsub).deliver_now
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
